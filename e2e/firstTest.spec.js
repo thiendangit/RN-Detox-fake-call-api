ee8550e9e922d986f3422d31ce7d4e121672e1ec
Dang Nguyen <hai.vo@aleph-labs.com>
2022-06-06 18:15:41 +0700

    fix: fix error when run detox test

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -9,11 +9,11 @@ describe("Example", () => {
   })
 
   it("should have welcome screen", async () => {
-    await expect(element(by.id("WelcomeScreen"))).toBeVisible()
+    await expect(element(by.id("DemoListScreen"))).toBeVisible()
   })
 
   it("should go to next screen after tap", async () => {
-    await element(by.id("next-screen-button")).tap()
+    await element(by.id("next-screen-button")).tap();
     await expect(element(by.id("DemoScreen"))).toBeVisible()
   })
 })
